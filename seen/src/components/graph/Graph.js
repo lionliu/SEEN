e4029cf110e9e49dcf92c08077d6dc78ff9347df
Leao Liu <llm2@cin.ufpe.br>
2020-03-12 16:31:42 -0300

    AP updating dynamically

--- seen/src/components/graph/Graph.js
+++ seen/src/components/graph/Graph.js
@@ -65,12 +65,11 @@ var myTool = d3.select("body")
     .style("display", "none");
 
 
-
-
 class Graph extends Component {
 
     state = {
         Aps: [],
+        Events: [],
         isFetching: false
     }
 
@@ -99,15 +98,16 @@ class Graph extends Component {
     }
 
     componentDidMount() {
-        // apis.getAllAps()
-        // .then(res => {
-        //     console.log(res.data);
-        //     this.setState({ Aps: res.data });
-        //     this.makeGraph();
-        // })
-        this.fetchAps();
+        apis.getAllAps()
+        .then(res => {
+            // console.log(res.data);
+            console.log('primeiro fetch')
+            this.setState({ Aps: res.data });
+            this.makeGraph();
+        })
+        // this.fetchAps();
         // this.makeGraph();
-        this.timer = setInterval(() => this.fetchAps(), 5000);
+        // this.timer = setInterval(() => this.fetchAps(), 5000);
     }
 
     componentWillUnmount() {
@@ -115,7 +115,6 @@ class Graph extends Component {
         this.timer = null;
     }
     
-    // Passar os atributos do makeGraph para fora para que ele possa reinicializar o d3 quando for chamado denovo no fetch
     makeGraph() {
         var data = toData(this.state.Aps);
         var nodes = data.nodes;
@@ -125,22 +124,15 @@ class Graph extends Component {
             .append('svg')
             .attr('width', '80vw')
             .attr('height', '82vh')
-            .call(d3.zoom()
-                .extent([[0, 0], [WIDTH, HEIGHT]])
-                .scaleExtent([1, 8])
-                .on("zoom", function () {
-                link.attr("transform", d3.event.transform)
-                node.attr("transform", d3.event.transform)
-            }))
+            // .attr("viewBox", [-WIDTH / 2, -HEIGHT / 2, WIDTH, HEIGHT])
+            // .call(d3.zoom()
+            //     .extent([[0, 0], [WIDTH, HEIGHT]])
+            //     .scaleExtent([1, 8])
+            //     .on("zoom", function () {
+            //     link.attr("transform", d3.event.transform)
+            //     node.attr("transform", d3.event.transform)
+            // }))
             .style('border', '1px solid black')
-
-        // var simulation = d3.forceSimulation(nodes) // Inicializa a simulação com o array de nodes
-        //     .force('charge', d3.forceManyBody().strength(-200)) // Faz com que os nós fiquem espaçados igualmente. O número negativo indica uma força de repulsao entre os nós.
-        //     .force('center', d3.forceCenter(WIDTH / 2, HEIGHT / 2)) // Indica a posição do centro
-        //     // .force('collision', d3.forceCollide().radius(function(d){return d.radius}))
-        //     .force('link', d3.forceLink().links(links).distance(70)) // Indica o array de arestas e o tamanho delas
-        //     // .force('link', d3.forceLink().links(links).distance(links.length * 3))
-        //     .on('tick', ticked) // Faz com que a simulacao rode
 
         var simulation = d3.forceSimulation(nodes)
             .force("link", d3.forceLink(links).id(d => d.id).links(links).distance(60))
@@ -150,87 +142,130 @@ class Graph extends Component {
             .force("y", d3.forceY())
             .on('tick', ticked);
 
-        // Vai gerar as arestas no DOM
         var link = d3.select('svg').append('g')
             .attr('class', 'links')
             .selectAll('line')
-            .data(links)
-            .enter()
-            .append('line')
-            .attr('stroke-width', 3)
-            .style('stroke', 'black')
 
-        // Gera os nodes no DOM
         var node = d3.select('svg').append('g')
             .attr('class', 'nodes')
             .selectAll('circle')
-            .data(nodes)
-        
-        node.exit().remove()
 
-        node = node.enter()
-            .append('circle')
-            .attr('r', d => d.radius)
-            .attr('fill', d => d.color)
-            .on("mouseover", function (d) {  //Mouse
-                d3.select(this)
-                    .transition()
-                    .duration(500)
-                    .style("cursor", "pointer")
-                    .attr("width", 60) //The bar becomes large
-                myTool.transition() // myTool é o body do html selecionado pelo d3
-                    .duration(500)
-                    .style('opacity', '1')
-                    .style('display', 'block');
-
-                myTool
-                    .html(
-                        "<div id ='teste' >Mac: " + d.mac + "<br>Status: " + d.status + "</div>"
-                    )
-                    .style("left", (d3.event.pageX - 90) + "px")
-                    .style("top", (d3.event.pageY - 60) + "px")
-            })
-            .on("mouseout", function (d) {  //Mouse event
-                d3.select(this)
-                    .transition()
-                    .duration(500)
-                    .style("cursor", "normal")
-                    .attr("width", 40)
-                myTool
-                    .transition()  //Opacity transition when the tooltip disappears
-                    .duration(500)
-                    .style("opacity", "0")
-                    .style("display", "none")  //The tooltip disappears
-            });
-
-        // var node = d3.select('svg').append('g')
-        //     .attr('class', 'nodes')
-        //     .selectAll('circle')
-            
-
-        
+        updateGraph(this.state.Aps)
 
         // Função necessaria para inicializar a simulacao
         function ticked() {
-            node.data(nodes)
-            link.data(links)
+            // node.data(nodes)
+            // link.data(links)
 
-            // With circles
             node.attr("cx", function (d) { return d.x; })
                 .attr("cy", function (d) { return d.y; })
-
-            // To keep within the borders
-            // node.attr("cx", function (d) { return d.x = Math.max(d.radius, Math.min(width - d.radius, d.x)); })
-            //     .attr("cy", function (d) { return d.y = Math.max(d.radius, Math.min(height - d.radius, d.y)); });
-
-            // With Images
-            // node.attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; })
 
             link.attr("x1", function (d) { return d.source.x; })
                 .attr("y1", function (d) { return d.source.y; })
                 .attr("x2", function (d) { return d.target.x; })
                 .attr("y2", function (d) { return d.target.y; });
         }
+
+        function updateGraph(Aps) {
+            let data = toData(Aps);
+            let nodes = data.nodes;
+            let links = data.links;
+            console.log(nodes);
+
+            // const old = new Map(node.data().map(d => [d.id, d]));
+            // nodes = nodes.map(d => Object.assign(old.get(d.id) || {}, d));
+            // links = links.map(d => Object.assign({}, d));
+
+            node = node.data(nodes, d => d.id);
+            // Post e delete dos aparelhos estao bugados. No post ele vem como azul e no delete ele sai, mas torna um AP verde.
+            node.exit().remove();
+
+            node = node.enter()
+                .append('circle')
+                .attr('r', d => d.radius)
+                .attr('fill', d => d.color)
+                .on("mouseover", function (d) {  //Mouse
+                    d3.select(this)
+                        .transition()
+                        .duration(500)
+                        .style("cursor", "pointer")
+                        .attr("width", 60) //The bar becomes large
+                    myTool.transition() // myTool é o body do html selecionado pelo d3
+                        .duration(500)
+                        .style('opacity', '1')
+                        .style('display', 'block');
+
+                    myTool
+                        .html(
+                            "<div id ='teste' >Mac: " + d.mac + "<br>Status: " + d.status + "</div>"
+                        )
+                        .style("left", (d3.event.pageX - 90) + "px")
+                        .style("top", (d3.event.pageY - 60) + "px")
+                })
+                .on("mouseout", function (d) {  //Mouse event
+                    d3.select(this)
+                        .transition()
+                        .duration(500)
+                        .style("cursor", "normal")
+                        .attr("width", 40)
+                    myTool
+                        .transition()  //Opacity transition when the tooltip disappears
+                        .duration(500)
+                        .style("opacity", "0")
+                        .style("display", "none")  //The tooltip disappears
+                })
+                .merge(node);
+
+
+
+            link = link.data(links);
+            link.exit().remove();
+
+            link = link.enter()
+            .append('line')
+            .attr('stroke-width', 3)
+            .style('stroke', 'black')
+            .merge(link);
+
+            simulation.nodes(nodes)
+                .force("link", d3.forceLink(links).id(d => d.id).links(links).distance(60))
+                .force("charge", d3.forceManyBody().strength(-150))
+                .force('center', d3.forceCenter(WIDTH / 2, HEIGHT / 2))
+                // .force("x", d3.forceX())
+                // .force("y", d3.forceY())
+                // .on('tick', ticked);
+            
+            simulation.alpha(1).restart();
+
+
+        }
+
+        var fetchAps = setInterval(() => {
+            this.setState({ ...this.state, isFetching: true });
+            apis.getAllAps()
+                .then(res => {
+
+                    this.setState({ ...this.state, isFetching: false, })
+                    // console.log(this.state.Aps)
+                    console.log('Print do fetch')
+                    // if(this.state.firstFetch) {
+                    //     this.setState({ ...this.state, firstFetch: false })
+                    //     this.makeGraph()
+                    // }
+                    let newData = res.data
+                    if (!_.isEqual(newData, this.state.Aps)) {
+                        this.setState({ Aps: res.data })
+                        // console.log(this.state.Aps);
+                        console.log('State')
+                        console.log(this.state.Aps);
+                        updateGraph(this.state.Aps)
+                    }
+                })
+                .catch(e => {
+                    console.log(e);
+                    this.setState({ ...this.state, isFetching: false });
+                });
+        }, 5000)
     }
 
     render() {
